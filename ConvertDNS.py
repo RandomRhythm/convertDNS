397fcf89db7a4c027d58dc7f4136053fbae0b896
Ryan B <randomrhythm@rhythmengineering.com>
2021-10-21 02:24:01 +0000

    Move dns conversion code to function. Check bounds on domain location in CSV row
    Signed-off-by: Ryan B <randomrhythm@rhythmengineering.com>

--- ConvertDNS.py
+++ ConvertDNS.py
@@ -3,8 +3,8 @@ import io
 import re
 import csv
 outputEncoding = "utf-8"
-strinputFile = "g:\\test\dga.txt" #Input file to process
-strOutPath = "g:\\test\\dga_formatted.txt" #output file
+strinputFile = "c:\\test\\domains.txt" #Input file to process
+strOutPath = "c:\\test\\domains_formatted.txt" #output file
 boolQuoteOutput = False #add quotes around each field
 boolAddEscape = False #add escape character
 domainLoc = 0
@@ -15,21 +15,27 @@ def writeCSV(fHandle, rowOut):
   else:
     fHandle.write(rowOut.replace("|", ",") + "\n")
 
+def dnsConvert(strDomainName, boolAddEscapeChar):
+  arrayDomain = re.split("\.", strDomainName)
+  outdomain = ""
+  for subdomain in arrayDomain:
+    if outdomain == "":
+      outdomain = "(" + str(len(subdomain)) + ")" + subdomain
+    else:
+      outdomain = outdomain + "(" + str(len(subdomain)) + ")" + subdomain
+  if boolAddEscapeChar == True:
+    outdomain= outdomain.replace("(","\\(").replace(")","\\)")
+    outdomain= outdomain + "\(0\)"
+  else:
+    outdomain= outdomain + "(0)"
+  return outdomain
+  
 with io.open(strOutPath, "w", encoding=outputEncoding) as f:
   intRowCount = 0;
   with open(strinputFile, "rt", encoding="utf-8") as csvfile: #, encoding="utf-16"
       reader = csv.reader(csvfile, delimiter=',', quotechar='\"')
       for row in reader:
-        tmpDomain = row[domainLoc]
-        arrayDomain = re.split("\.", tmpDomain)
-        outdomain = ""
-        for subdomain in arrayDomain:
-          if outdomain == "":
-            outdomain = "(" + str(len(subdomain)) + ")" + subdomain
-          else:
-            outdomain = outdomain + "(" + str(len(subdomain)) + ")" + subdomain
-        if boolAddEscape == True:
-          outdomain= outdomain.replace("(","\\(").replace(")","\\)")
-          writeCSV(f,outdomain + "\(0\)")
-        else:
-          writeCSV(f,outdomain + "(0)")
+        if len(row) > domainLoc:
+          tmpDomain = row[domainLoc]
+          tmpDomain = dnsConvert(tmpDomain, boolAddEscape)
+          writeCSV(f,tmpDomain)
